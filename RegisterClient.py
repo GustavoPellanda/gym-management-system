33bcd01e3ce547f3949f274fb358773eed866f40
GustavoPellanda <gusfipe@gmail.com>
2025-01-27 16:16:39 -0300

    Banco de dados salva clientes.

--- RegisterClient.py
+++ RegisterClient.py
@@ -6,21 +6,14 @@ class RegisterClient:
     def __init__(self, database_manager):
         self.database_manager = database_manager
 
-    def SetClientName(self, cpf, name):
-        """
-        Define o nome do cliente e registra no sistema.
-        """
-        if not cpf or not name:
-            raise ValueError("CPF e Nome são obrigatórios.")
-        self.database_manager.RegisterClient(cpf, {"name": name, "workout": None})
-
     def SetClientInfo(self, cpf, info):
         """
-        Define informações adicionais para o cliente.
-        Registra o cliente se ele não existir.
+        Define as informações do cliente e registra ou atualiza conforme necessário.
         """
         if not cpf or not info:
             raise ValueError("CPF e informações são obrigatórios.")
+        
+        # Se o cliente não existir, cria um novo
         if self.database_manager.SearchClient(cpf) is None:
             self.database_manager.RegisterClient(cpf, info)
         else:
